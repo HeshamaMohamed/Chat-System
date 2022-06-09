9006e926765a228a47e123d0f1117d514a0c93a8
heshamamohamed <heshamamohamed323@gmail.com>
2022-06-09 09:16:19 +0200

    Added Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :applications,  only: %i[index show update create], param: :token do
+        resources :chats,       only: %i[index show update create], param: :chat_number do
+          member do
+            resources :messages,  only: %i[index show update create], param: :message_number
+          end
+        end
+      end
+    end
+  end
 end
