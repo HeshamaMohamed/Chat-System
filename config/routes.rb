e167000165ec95b8755f36231411b70c46610f1c
heshamamohamed <heshamamohamed323@gmail.com>
2022-06-11 07:28:23 +0200

    Added Searching routes for chats

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
       resources :applications,  only: %i[index show update create], param: :token do
         resources :chats,       only: %i[index show update create], param: :chat_number do
           member do
-            resources :messages,  only: %i[index show update create], param: :message_number
+            post 'search', to: 'chats#search'
+            resources :messages, only: %i[index show update create], param: :message_number
           end
         end
       end
