af09e1325c3ae2b451d7e5ffb788b3460869211a
heshamamohamed <heshamamohamed323@gmail.com>
2022-06-11 07:30:11 +0200

    Added searching method in Chats controller

--- app/controllers/api/v1/chats_controller.rb
+++ app/controllers/api/v1/chats_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ChatsController < Api::V1::ApisController
-  before_action :set_chat, only: %i[show update]
+  before_action :set_chat, only: %i[show update search]
 
   def index
     @chats = Chat.where(application_params)
@@ -17,9 +17,31 @@ class Api::V1::ChatsController < Api::V1::ApisController
     render json: { chat_number: chat_number }
   end
 
+  def search
+    query = params[:query]
+    search_matches =
+      Message.search query,
+                     fields: [:body],
+                     match: :text_middle,
+                     where: { application_token: @current_chat.application_token,
+                              chat_number: @current_chat.chat_number },
+                     order: { _score: :desc }, # most relevant first
+                     load: false # load from elastic search instead of db
+
+    if !search_matches.empty?
+      render json: search_matches.results, only: %i[body message_number created_at]
+    else
+      render json: { results: 'No messages found that matches your search.' }
+    end
+  end
+
   private
 
   def application_params
     params.permit(:application_token)
+  end
+
+  def search_params
+    params.permit(:query)
   end
 end
