b7d3e362e2a04c9b049ebf4d75b8de5c6db43985
heshamamohamed <heshamamohamed323@gmail.com>
2022-06-11 07:27:55 +0200

    Added Searchkick to Message Model

--- app/models/Message.rb
+++ app/models/Message.rb
@@ -1,4 +1,11 @@
 class Message < ApplicationRecord
+  searchkick searchable: :body, # searches only in messages body.
+             callbacks: :async,
+             text_middle: [:body], # matches any part of the text
+             deep_paging: true # show all results instead of paginating first 10,000
+
+  after_commit :reindex
+
   validates :application_token, :chat_number, :body, presence: true
 
   belongs_to :chat, foreign_key: %i[application_token chat_number],
